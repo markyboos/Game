8f94456d7c96206504b67b469ea84ccbdc668dda
James Kember <devjkgames@gmail.com>
2013-03-26 22:10:49 +0000

    deleted loads of stuff not needed for the game

---
+++ src/com/game/thrones/engine/RumorsAction.java
@@ -0,0 +1,25 @@
+
+package com.game.thrones.engine;
+
+import com.game.thrones.model.hero.Hero;
+import com.game.thrones.model.hero.Item;
+import com.game.thrones.model.piece.Piece;
+
+/**
+ *
+ * @author James
+ */
+public class RumorsAction extends AbstractAction { 
+    
+    public RumorsAction(final Piece piece) {
+        super(piece);
+    }
+
+    public void execute() {
+        
+        Hero hero = (Hero)piece;
+        
+        hero.addItem(new Item(1));
+    }
+
+}
